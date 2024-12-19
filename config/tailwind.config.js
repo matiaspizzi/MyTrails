57a9492fb023783c1b03029b5e8be1eb1b6b6fd6
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-12-19 18:54:04 -0300

    colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      colors: {
+        'primary': '#212529',
+        'secondary': '#9fef00',
+        'tertiary': '#202837',
+      },
     },
   },
   plugins: [

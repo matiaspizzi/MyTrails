3e377396cb0e213fa1d3a759433479f1b9f34d79
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-12-21 17:44:27 -0300

    home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
-  allow_unauthenticated_access
-
   def index
+    case current_user.role
+    when "admin"
+      @objectives = Objective.includes(:employee, :rater).all
+      @objective = Objective.new
+      render "home/admin_dashboard"
+    when "leader"
+      @objectives = Objective.includes(:employee).where(employee_id: current_user.employees.pluck(:id))
+      @objective = Objective.new
+      render "home/leader_dashboard"
+    when "employee"
+      @objectives = Objective.where(employee: current_user)
+      @objective = Objective.new
+      render "home/employee_dashboard"
+    else
+      redirect_to root_path, alert: "Rol no reconocido"
+    end
   end
 end

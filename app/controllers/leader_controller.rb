81741bf878f3ccaf4ee258cc02aa61d39f0bfbef
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-12-24 01:18:52 -0300

    leader done

--- app/controllers/leader_controller.rb
+++ app/controllers/leader_controller.rb
@@ -1,7 +1,7 @@
 class LeaderController < ApplicationController
   def dashboard
     if current_user.role != "leader"
-      redirect_to root_path, alert: "No tienes permisos para acceder a esta página"
+      redirect_to root_path, alert: "You don't have permission to access this page."
     end
     if params[:query].present?
         @objectives = Objective.joins("INNER JOIN users ON users.id = objectives.employee_id")
@@ -14,10 +14,24 @@ class LeaderController < ApplicationController
                                   OR users.email_address ILIKE :query 
                                   OR CAST(objectives.estimated_completion_at AS TEXT) ILIKE :query", 
                                  query: "%#{params[:query]}%"
-                               )
+                               ).order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
     else
-      @objectives = Objective.includes(:employee).where(employee_id: current_user.employees.pluck(:id))
+      # objectives with status In Review, first 
+      @objectives = Objective.includes(:employee)
+      .where(employee_id: current_user.employees.pluck(:id))
+      .order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
     end
-    render "home/leader_dashboard"
+    render "leader/dashboard"
+  end
+
+  def objective
+    @objective = Objective.includes(:employee, :rater).find(params[:id])
+    if @objective == nil
+      redirect_to leader_dashboard_path, alert: 'We could not find the objective you are looking for.'
+    end
+    if current_user.role != "leader" || !current_user.employees.include?(@objective.employee)
+      redirect_to leader_dashboard_path, alert: "You don't have permission to access this page."
+    end
+    render "leader/objective"
   end
 end

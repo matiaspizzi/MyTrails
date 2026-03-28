966204132beb5088b65b8de21c02d79e878d4425
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-03-28 20:36:22 -0300

    add: pagy pagination to admins, leaders, and home controllers

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,31 +2,23 @@ class AdminsController < ApplicationController
   def dashboard
     authorize current_user, :view_admin_dashboard?, policy_class: UserPolicy
     if params[:query].present?
-        @objectives = Objective.joins("INNER JOIN users ON users.id = objectives.employee_id")
-                                .where(
-                                 "objectives.title ILIKE :query
-                                  OR objectives.description ILIKE :query
-                                  OR users.name ILIKE :query
-                                  OR users.surname ILIKE :query
-                                  OR users.email_address ILIKE :query
-                                  OR CAST(objectives.estimated_completion_at AS TEXT) ILIKE :query",
-                                 query: "%#{params[:query]}%"
-                               ).order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
+        @pagy, @objectives = pagy(
+          Objective.joins("INNER JOIN users ON users.id = objectives.employee_id")
+                   .where(
+                     "objectives.title ILIKE :query
+                      OR objectives.description ILIKE :query
+                      OR users.name ILIKE :query
+                      OR users.surname ILIKE :query
+                      OR users.email_address ILIKE :query
+                      OR CAST(objectives.estimated_completion_at AS TEXT) ILIKE :query",
+                     query: "%#{params[:query]}%"
+                   ).order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
+        )
     else
-      # objectives with status In Review, first
-      @objectives = Objective.includes(:employee).order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
+      @pagy, @objectives = pagy(
+        Objective.includes(:employee).order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
+      )
     end
     render "admin/dashboard"
-  end
-
-  def objective
-    @objective = Objective.includes(:employee, :rater).find(params[:id])
-    if @objective == nil
-      redirect_to admin_dashboard_path, alert: "We could not find the objective you are looking for."
-    elsif current_user.role != "admin"
-      redirect_to admin_dashboard_path, alert: "You don't have permission to access this page."
-      return
-    end
-    render "admin/objective"
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @objective = Objective.new
-    if current_user.role == "admin"
-      @objectives = Objective.includes(:employee, :rater).all
+    if current_user.admin?
+      @pagy, @objectives = pagy(Objective.includes(:employee, :rater).order(created_at: :desc))
       render "admin/dashboard"
     else
-      @objectives = Objective.where(employee: current_user).includes(:employee, :rater).order(status: :asc, created_at: :asc)
+      @pagy, @objectives = pagy(Objective.where(employee: current_user).includes(:employee, :rater).order(status: :asc, created_at: :asc))
       render "home/employee_dashboard"
     end
   end

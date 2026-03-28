0a31a5f4a35bb124fa265775fcf9393e130d5e60
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-03-28 20:58:46 -0300

    i18n: objectives

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -17,12 +17,12 @@ class ObjectivesController < ApplicationController
 
     if @objective.save
       respond_to do |format|
-        format.html { redirect_to root_path, notice: "Objective was successfully created." }
+        format.html { redirect_to root_path, notice: t("objectives.created") }
         format.turbo_stream
       end
     else
       respond_to do |format|
-        format.html { redirect_to root_path, alert: "Failed to create objective." }
+        format.html { redirect_to root_path, alert: t("objectives.create_failed") }
         format.turbo_stream { render :new }
       end
     end
@@ -31,28 +31,28 @@ class ObjectivesController < ApplicationController
   def update
     authorize @objective
     if @objective.update(objective_params)
-      redirect_to root_path, notice: "Objective status updated successfully."
+      redirect_to root_path, notice: t("objectives.updated")
     else
-      redirect_to root_path, alert: "Failed to update objective status."
+      redirect_to root_path, alert: t("objectives.update_failed")
     end
   end
 
   def destroy
     authorize @objective
     if @objective.status == "Done"
-      redirect_to root_path, alert: "You can't delete a completed objective."
+      redirect_to root_path, alert: t("objectives.delete_denied")
       return
     end
     @objective.destroy
-    redirect_to root_path, notice: "Objective deleted successfully."
+    redirect_to root_path, notice: t("objectives.deleted")
   end
 
   def rate
     authorize @objective
     if @objective.update(rating: params[:rating], status: :Done, rated_by: current_user.id)
-      flash[:notice] = "Rating updated successfully."
+      flash[:notice] = t("objectives.rated")
     else
-      flash[:alert] = "Failed to update rating."
+      flash[:alert] = t("objectives.rate_failed")
     end
     redirect_back(fallback_location: root_path)
   end
@@ -62,9 +62,9 @@ class ObjectivesController < ApplicationController
     @objective.rating = nil
     @objective.rated_by = nil
     if @objective.save
-      redirect_to root_path, notice: "Rating removed successfully."
+      redirect_to root_path, notice: t("objectives.unrated")
     else
-      redirect_to root_path, alert: "Failed to remove rating."
+      redirect_to root_path, alert: t("objectives.unrate_failed")
     end
   end
 

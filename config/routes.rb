18ccc3e7f063beb94ac91163498ec0221c0a950b
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-03-28 20:44:05 -0300

    refactor: RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,37 @@
 Rails.application.routes.draw do
-  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  
-  # get 'confirm_registration', to: 'registrations#confirm'
-  resource :registration, only: [:new, :create]
-  resources :passwords, param: :token
-  resources :objectives, only: [:create, :update, :destroy]
+  # Authentication
+  resource :registration, only: [ :new, :create ]
+  resource :session, only: [ :new, :create, :destroy ]
+  resources :passwords, param: :token, only: [ :new, :create, :edit, :update ]
 
-
-  get 'sign_in', to: 'sessions#new', as: :new_session
-  post 'sign_in', to: 'sessions#create', as: :create_session
-  delete 'sign_out', to: 'sessions#destroy', as: :destroy_session
-  
-  get 'account', to: 'users#account', as: :account
-  resources :users do
+  # Objectives
+  resources :objectives, only: [ :create, :update, :destroy ] do
     member do
-      patch :update_profile_image
+      post :rate
+      post :unrate
+      get  :details
+    end
+  end
+
+  # Users & account
+  get   "account",         to: "users#account",        as: :account
+  get   "users_roles",     to: "users#show_all",        as: :users_roles
+  patch "users_roles/:id", to: "users#set_user_role",   as: :set_user_role
+  resources :users, only: [] do
+    member do
+      patch  :update_profile_image
       delete :delete_profile_image
     end
   end
-  
-  post 'objective/rate', to: 'objectives#rate', as: :rate_objective
-  get 'objective/:id', to: 'objectives#details', as: :objective_details
 
-  get 'leader_dashboard', to: 'leaders#dashboard', as: :leader_dashboard
-  
-  get 'admin_dashboard', to: 'admins#dashboard', as: :admin_dashboard
+  # Dashboards
+  get "leader_dashboard", to: "leaders#dashboard", as: :leader_dashboard
+  get "admin_dashboard",  to: "admins#dashboard",  as: :admin_dashboard
 
-  get 'users_roles', to: 'users#show_all', as: :users_roles
+  # Leaderships (admin only)
+  resources :leaderships, only: [ :index, :create, :destroy ]
 
-  patch 'users_roles.:id', to: 'users#set_user_role', as: :set_user_role
-
-  get 'leadership', to: 'leaderships#show', as: :leadership
-  post 'leadership', to: 'leaderships#create', as: :leadership_create
-  delete 'leadership.:id', to: 'leaderships#destroy', as: :leadership_destroy
-
-
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "home#index"

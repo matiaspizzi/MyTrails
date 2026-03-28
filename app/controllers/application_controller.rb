31b57ec8f65a0cb9da5d68c8b9c91daea167e318
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-03-28 19:50:10 -0300

    add: Pundit to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  include Pundit::Authorization
+
   allow_browser versions: :modern
+
+  rescue_from Pundit::NotAuthorizedError, with: :handle_not_authorized
 
   helper_method :current_user
 
   def current_user
     Current.session&.user
+  end
+
+  private
+
+  def handle_not_authorized
+    redirect_to root_path, alert: "You don't have permission to access this page."
   end
 end

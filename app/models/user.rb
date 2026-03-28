1ee6b8b43674a97d5ba5d2efb5749489f74a531a
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-03-28 20:13:14 -0300

    refactor: user model - active storage + deliver_later

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :confirmation_token
   has_many :sessions, dependent: :destroy
+  has_one_attached :profile_image
 
-  # Associations with inverse_of and dependent options
   has_many :objectives, foreign_key: :employee_id, class_name: "Objective", inverse_of: :employee
 
   has_many :leaderships, foreign_key: :leader_id, class_name: "Leadership", inverse_of: :leader, dependent: :destroy
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validate :validate_profile_image_size
+  validate :profile_image_size
 
   def confirm!
     update!(confirmed_at: Time.current, confirmation_token: nil)
@@ -26,18 +26,13 @@ class User < ApplicationRecord
 
   def send_confirmation_instructions
     regenerate_confirmation_token
-    UserMailer.confirmation_instructions(self).deliver_now
+    UserMailer.confirmation_instructions(self).deliver_later
   end
 
-  def validate_profile_image_size
-    return if profile_image.blank?
+  private
 
-    # Decode Base64 to calculate the size
-    size_in_bytes = Base64.decode64(profile_image).bytesize
-    max_size_in_bytes = 1.megabyte
-
-    if size_in_bytes > max_size_in_bytes
-      errors.add(:profile_image, "The image is too large. The maximum size is 1MB.")
-    end
+  def profile_image_size
+    return unless profile_image.attached? && profile_image.blob.byte_size > 1.megabyte
+    errors.add(:profile_image, "The image is too large. The maximum size is 1MB.")
   end
 end

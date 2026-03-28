bf2b54dce672d2377228e7614241034576731f1e
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-03-28 20:28:54 -0300

    refactor: use search/in_review_first scopes in admins and leaders controllers

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,24 +1,10 @@
 class LeadersController < ApplicationController
   def dashboard
     authorize current_user, :view_leader_dashboard?, policy_class: UserPolicy
-    if params[:query].present?
-        @objectives = Objective.joins("INNER JOIN users ON users.id = objectives.employee_id")
-                               .where(employee_id: current_user.employees.pluck(:id))
-                               .where(
-                                 "objectives.title ILIKE :query
-                                  OR objectives.description ILIKE :query
-                                  OR users.name ILIKE :query
-                                  OR users.surname ILIKE :query
-                                  OR users.email_address ILIKE :query
-                                  OR CAST(objectives.estimated_completion_at AS TEXT) ILIKE :query",
-                                 query: "%#{params[:query]}%"
-                               ).order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
-    else
-      # objectives with status In Review, first
-      @objectives = Objective.includes(:employee)
-      .where(employee_id: current_user.employees.pluck(:id))
-      .order(Arel.sql("CASE WHEN status = 2 THEN 0 ELSE 1 END"), :status, :created_at)
-    end
+    @objectives = Objective.includes(:employee)
+                           .where(employee: current_user.employees)
+                           .then { params[:query].present? ? _1.search(params[:query]) : _1 }
+                           .in_review_first
     render "leader/dashboard"
   end
 end
